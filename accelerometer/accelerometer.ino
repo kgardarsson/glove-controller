14e0a602f7df9b9bcad4f465f26a04f110d32fd6
malteerasmussen <malteerasmussen@gmail.com>
2020-05-18 14:58:13 +0200

    Update accelerometer.ino

--- accelerometer/accelerometer.ino
+++ accelerometer/accelerometer.ino
@@ -27,34 +27,79 @@
 // these constants describe the pins. They won't change:
 const int groundpin = 18;             // analog input pin 4 -- ground
 const int powerpin = 19;              // analog input pin 5 -- voltage
-const int xpin = A3;                  // x-axis of the accelerometer
-const int ypin = A2;                  // y-axis
-const int zpin = A1;                  // z-axis (only on 3-axis models)
+const int xpin = A0;                  // x-axis of the accelerometer
+const int ypin = A1;                  // y-axis
+const int zpin = A2;                  // z-axis (only on 3-axis models)
+
+
+// Define the number of samples to keep track of. The higher the number, the
+// more the readings will be smoothed, but the slower the output will respond to
+// the input. Using a constant rather than a normal variable lets us use this
+// value to determine the size of the readings array.
+const int numReadings = 30;
+
+
+int xval;
+int yval;
+int zval;
+int accel;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+int startVal;                   // start value for calibrating
 
 void setup() {
   // initialize the serial communications:
   Serial.begin(9600);
 
-  // Provide ground and power by using the analog inputs as normal digital pins.
-  // This makes it possible to directly connect the breakout board to the
-  // Arduino. If you use the normal 5V and GND pins on the Arduino,
-  // you can remove these lines.
-//  pinMode(groundpin, OUTPUT);
-//  pinMode(powerpin, OUTPUT);
-//  digitalWrite(groundpin, LOW);
-//  digitalWrite(powerpin, HIGH);
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+
+  startVal = analogRead(xpin);
 }
 
 void loop() {
+  xval = analogRead(xpin);
+  yval = analogRead(ypin);
+  zval = analogRead(zpin);
+
+  //  accel = sqrt(sq(xval)+sq(yval)+sq(zval));
+
+
+
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(xpin);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+
+
+
+
+
   // print the sensor values:
-  Serial.println(analogRead(xpin));
-//  // print a tab between values:
-//  Serial.print("\t");
-//  Serial.print(analogRead(ypin));
-//  // print a tab between values:
-//  Serial.print("\t");
-//  Serial.print(analogRead(zpin));
-//  Serial.println();
+  Serial.println(average - startVal);
+  //  // print a tab between values:
+  //  Serial.print("\t");
+  //  Serial.print(analogRead(ypin));
+  //  // print a tab between values:
+  //  Serial.print("\t");
+  //  Serial.print(analogRead(zpin));
+  //  Serial.println();
   // delay before next reading:
-  delay(20);
+  delay(1);
 }
