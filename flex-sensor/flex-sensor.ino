c2c5e76acef8c1479bb6254ef7d2ece297dc5f7f
malteerasmussen <malteerasmussen@gmail.com>
2020-05-18 14:06:02 +0200

    Update flex-sensor.ino

--- flex-sensor/flex-sensor.ino
+++ flex-sensor/flex-sensor.ino
@@ -2,7 +2,7 @@
    Fade an LED with a flex sensor
    More info: http://www.ardumotive.com/how-to-use-a-flex-sensor-en.html
    Dev: Michalis Vasilakis // Date: 9/7/2015 // www.ardumotive.com  */
-   
+
 
 //Constants:
 const int ledPin = 3;   //pin 3 has PWM funtion
@@ -16,29 +16,34 @@ const int accXPin = A0;
 int value1; //save analog value
 int value2;
 int value3;
+int lastValue3 = 0;
 int accXValue;
 
-void setup(){
+void setup() {
   pinMode(ledPin, OUTPUT);  //Set pin 3 as 'output'
   Serial.begin(9600);       //Begin serial communication
 }
 
-void loop(){
+void loop() {
   value1 = analogRead(flexPin1);         //Read and save analog value from potentiometer
   value2 = analogRead(flexPin2);
   value3 = analogRead(flexPin3);
   accXValue = analogRead(accXPin);
   Serial.print("flex1 ");
+
   Serial.println(value1);               //Print value
   Serial.print("flex2 ");
-  Serial.println(value2); 
+  Serial.println(value2);
+  int flex3 = map(value3, 87, 79, 0, 1) * 2;
   Serial.print("flex3 ");
-  Serial.println(value3);
+  Serial.println(flex3);
+  lastValue3 = value3;
+
   Serial.print("accX ");
   Serial.println(accXValue);
 
-  
- // value1 = map(value1, 700, 900, 0, 255);//Map value 0-1023 to 0-255 (PWM)
- // analogWrite(ledPin, value1);          //Send PWM value to led
- // delay(100);                          //Small delay
+
+  value1 = map(value1, 700, 900, 0, 255);//Map value 0-1023 to 0-255 (PWM)
+  analogWrite(ledPin, value1);          //Send PWM value to led
+  delay(100);                          //Small delay
 }
